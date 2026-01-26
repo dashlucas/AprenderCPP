71322141d17a25306b835b2666f7be9981e3de98
Burns <39342555+dashlucas@users.noreply.github.com>
2026-01-26 12:11:53 -0300

    Aprendizados com o livro

--- CPP/CPP1/main.cpp
+++ CPP/CPP1/main.cpp
@@ -1,16 +1,19 @@
-#include <iostream>
+#include <iostream> // Substitui a parte de input/output do PPP.h
+#include <string>   // Substitui a parte de strings do PPP.h
+#include <cmath> // Adicione isto para usar sqrt
 
-using namespace std;
+using namespace std; // O PPP.h fazia isso automaticamente
 
-int main()
+int main() // read and compare names
 {
-    cout << "***************\n";
-    cout << " *           *\n";
-    cout << "  *  BURNS  *\n";
-    cout << "   *       *\n";
-    cout << "    *     *\n";
-    cout << "     *   *\n";
-    cout << "      * *\n";
-    cout << "       *\n";
-    return 0;
+cout << "Please enter two names\n";
+string first;
+string second;
+cin >> first >> second; // read two str ings
+if (first == second)
+cout << "that's the same name twice\n";
+if (first < second)
+cout << first << " is alphabetically before " << second <<'\n';
+if (first > second)
+cout << first << " is alphabetically after " << second <<'\n';
 }
